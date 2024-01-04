06cc07d38f3d98f4955a2a0be7d850dd90e4f8bb
unknown <bdcordova@utpl.edu.ec>
2024-01-04 17:31:53 -0500

    cambios

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,9 @@
  */
 package problemas;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +19,35 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double[] notas = new double[4];
+        int opcion1;
+        boolean bandera = true;
+        while (bandera) {
+            for (int i = 0; i < notas.length; i++) {
+                System.out.println("Ingrese las cuatro notasa");
+                notas[i] = entrada.nextInt();
+                obtenerPromedioCualitativo(notas)
+            }
+
+            System.out.println("Si quiere terminar el proceso digite 1");
+            opcion1 = entrada.nextInt();
+            if (opcion1 == 1) {
+                bandera = false;
+            }
+
+        }
     }
-    
+
+    public static String obtenerPromedioCualitativo() {
+        Scanner entrada = new Scanner(System.in);
+        double promedio;
+        double area;
+        System.out.println("Ingresar el lado del cuadrado");
+        lado = entrada.nextDouble();
+        area = lado * lado * lado * lado;
+        return promedio
+    }
+
 }
